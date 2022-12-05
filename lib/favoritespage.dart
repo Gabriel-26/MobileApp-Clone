25fee869f5844f1367f11ce4d65e4eb7cd281b3c
Angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2022-12-04 19:17:17 -0800

    Favorites Page Minor Changes

--- lib/favoritespage.dart
+++ lib/favoritespage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_homepage/homepage.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:login_homepage/login_page.dart';
 
 // ignore_for_file: prefer_const_literals_to_create_immutables
 // ignore_for_file: prefer_const_constructors
@@ -15,6 +17,33 @@ class _FavoritesPage extends State<FavoritesPage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      bottomNavigationBar: GNav(
+        backgroundColor: Colors.amber.shade500,
+        tabs: [
+          GButton(icon: Icons.home_sharp,
+            text: 'Home',
+            onPressed: (){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context){
+                    return const HomePage();
+                  })
+              );
+            },
+          ),
+          GButton(icon: Icons.thumb_up_alt,
+            text: 'Favorites',
+            onPressed: (){}
+          ),
+          GButton(icon: Icons.timer_sharp,
+            text: 'Recent',
+            onPressed: (){},
+          ),
+          GButton(icon: Icons.download_rounded,
+            text: 'Downloads',
+            onPressed: (){},
+          ),
+        ],
+      ),
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.amber[400],
@@ -29,7 +58,7 @@ class _FavoritesPage extends State<FavoritesPage>{
         leading: IconButton(
             icon: const Icon(Icons.menu_sharp),
             onPressed:(){ Navigator.pop(context,
-              MaterialPageRoute(builder:(context)=> HomePage()),
+              MaterialPageRoute(builder:(context)=> LoginPage()),
             );
             }
         ),
