3f37a4beb544b07d67a5140e9b608fd167ae374b
Gabriel <gabriel.tejana26@gmail.com>
2022-12-06 22:36:18 +0800

    favorites page progress

--- lib/favoritespage.dart
+++ lib/favoritespage.dart
@@ -19,9 +19,176 @@ class _FavoritesPage extends State<FavoritesPage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-    body:Center(
-        child: Text("FAVE"),
-    ),
+      body: Center(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              // screens.elementAt(currentIndex),
+
+              SizedBox(
+                height: 35,
+              ),
+
+
+                 Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                  ),
+                  child: Wrap(
+                    spacing: 12,
+                    runSpacing: 14,
+                    // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        height: 140,
+                        // alignment: Alignment.centerLeft,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: AspectRatio(
+                              aspectRatio: 6/5,
+                              child: Image(
+                                image: AssetImage('assets/naruto.png'),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 140,
+                        // alignment: Alignment.centerLeft,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: AspectRatio(
+                              aspectRatio:6/5,
+                              child: Image(
+                                image: AssetImage('assets/pokemon.png'),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 140,
+                        // alignment: Alignment.centerLeft,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: AspectRatio(
+                              aspectRatio: 6/5,
+                              child: Image(
+                                image: AssetImage('assets/hxh.png'),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 140,
+                        // alignment: Alignment.centerLeft,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: AspectRatio(
+                              aspectRatio: 6/5,
+                              child: Image(
+                                image: AssetImage('assets/db.png'),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 140,
+                        // alignment: Alignment.centerLeft,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: AspectRatio(
+                              aspectRatio: 6/5,
+                              child: Image(
+                                image: AssetImage('assets/naruto.png'),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 140,
+                        // alignment: Alignment.centerLeft,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: AspectRatio(
+                              aspectRatio: 6/5,
+                              child: Image(
+                                image: AssetImage('assets/naruto.png'),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 140,
+                        // alignment: Alignment.centerLeft,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: AspectRatio(
+                              aspectRatio: 6/5,
+                              child: Image(
+                                image: AssetImage('assets/naruto.png'),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 140,
+                        // alignment: Alignment.centerLeft,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: AspectRatio(
+                              aspectRatio: 6/5,
+                              child: Image(
+                                image: AssetImage('assets/naruto.png'),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+
+                  ),
+
+                ),
+
+
+
+  ]
+          ),
+        )
+      ),
     );
   }
 }
